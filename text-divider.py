06f7af00ec1fd30dac13a740a9c545d23c458b58
Moacir P. de Sá Pereira <moacir@gmail.com>
2016-10-15 01:05:35 -0400

    create the actual python module.

---
+++ text-divider.py
@@ -0,0 +1,9 @@
+# text-divider.py, © 2016, Moacir P. de Sá Pereira
+#
+# Available on github: https://github.com/muziejus/text-divider
+# 
+# A Python implementation of David Hoover’s Analyze Textual Divisions Spreadsheet:
+#
+# https://wp.nyu.edu/exceltextanalysis/analyzetextualdivisions/
+
+
